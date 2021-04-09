ad30358b4dfc0094fc4999ada5498e7996b3b45f
cat-bro <cathjb@gmail.com>
2021-04-09 18:57:26 +1000

    .

--- lib/galaxy/webapps/galaxy/api/tools.py
+++ lib/galaxy/webapps/galaxy/api/tools.py
@@ -212,6 +212,7 @@ class ToolsController(BaseAPIController, UsesVisualizationMixin):
         """
         if 'payload' in kwd:
             kwd = kwd.get('payload')
+
         log.debug('\n-----------------------------------------------------------------------------\n')
         log.debug('payload')
         log.debug(payload)
