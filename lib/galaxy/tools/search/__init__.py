5c955df11160d0ee5c16a6b937ea1502d5cbd174
cat-bro <cathjb@gmail.com>
2022-04-19 18:28:44 +1000

    .

--- lib/galaxy/tools/search/__init__.py
+++ lib/galaxy/tools/search/__init__.py
@@ -139,6 +139,7 @@ class ToolPanelViewSearch:
                 panel_has_tool = self.toolbox.panel_has_tool(tool, self.panel_view_id)
                 print(f"##### self.toolbox.get_tool(tool_id): {str(tool)}")
                 print(f"##### panel_has_tool: {panel_has_tool}")
+                print(f"##### tool is latest version: {tool.is_latest_version}")
                 if tool and tool.is_latest_version and panel_has_tool:
                     if tool.hidden:
                         # we check if there is an older tool we can return
