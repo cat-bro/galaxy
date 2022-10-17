702c1aaea168cb118f9908f5722f2b60d6d46cf8
cat-bro <cathjb@gmail.com>
2022-10-17 14:26:52 +1100

    .

--- lib/galaxy/web/framework/decorators.py
+++ lib/galaxy/web/framework/decorators.py
@@ -110,9 +110,9 @@ def require_admin(func):
                 return __api_error_dict(trans, status_code=403, err_code=error_codes.ADMIN_REQUIRED, err_msg=msg)
             else:
                 return trans.show_error_message(msg)
-        print("hello")
-        print(str(args))
-        print(str(kwargs))
+        log.debug("hello")
+        log.debug(str(args))
+        log.debug(str(kwargs))
         return func(self, trans, *args, **kwargs)
 
     return decorator
